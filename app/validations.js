128adbbd678c0089e58125330f3589b71176685b
marthexmit <matheusrug2010@gmail.com>
2022-10-21 14:32:28 -0300

    Completed project

---
+++ app/validations.js
@@ -0,0 +1,57 @@
+function verificaChuteIsValidate(chute){
+    const numero = +chute
+
+    if(chuteInvalido(numero)){
+        if (chuteInvalido(numero)) {
+            if (chute.toUpperCase() === "GAME OVER") {
+    
+                document.body.innerHTML =
+                    `
+                    <h2>Game Over!!!</h2>
+                    <h3>Pressione o botão para jogar novamente</h3>
+                    <button id="jogarNovamente" class="btn-jogar">Jogar Novamente</button>
+                    `
+                    document.body.style.backgroundColor = "black";
+            } else {
+    
+                elementoChute.innerHTML += '<div>Valor Inválido</div>';
+            }
+        }
+        
+    }
+
+    if (NumeroMaiorMenor (numero)){
+        elementoChute.innerHTML += `
+        <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogarNovamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div> `
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+
+    
+}
+
+function chuteInvalido(numero){
+    return Number.isNaN(numero)
+}
+
+function NumeroMaiorMenor(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogarNovamente'){
+        window.location.reload();
+    }
+})
